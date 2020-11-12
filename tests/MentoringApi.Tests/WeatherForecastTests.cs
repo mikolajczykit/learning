232a455e711bc501d184a318603b751d601653b3
mmik@softserveinc.com <mmik@softserveinc.com>
2020-11-12 15:08:10 +0100

    xUnit test project

---
+++ tests/MentoringApi.Tests/WeatherForecastTests.cs
@@ -0,0 +1,29 @@
+using MentoringApi.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace MentoringApi.Tests
+{
+    public class WeatherForecastTests
+    {
+        [Fact]
+        public void VerifyIfForecastDateIsBetweenFiveDaysTimeSpan()
+        {
+            Mock<ILogger<WeatherForecastController>> loggerMock = new Mock<ILogger<WeatherForecastController>>();
+
+            WeatherForecastController controller = new WeatherForecastController(loggerMock.Object);
+            IEnumerable<WeatherForecast> result = controller.Get();
+            
+            DateTime maxDateScope = DateTime.Now.AddDays(5);
+            DateTime minDateScope = DateTime.Now.AddDays(1);
+
+            bool allDatesBetweenExpectedScope = result.All(x => x.Date.Date >= minDateScope.Date && x.Date.Date <= maxDateScope.Date);
+
+            Assert.True(allDatesBetweenExpectedScope);
+        }
+    }
+}
